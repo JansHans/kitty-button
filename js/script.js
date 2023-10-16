077a22444b939f9068b31e07dc5f700a7bef6db6
Jenni Hanna <79390603+JansHans@users.noreply.github.com>
2023-10-16 10:21:45 -0400

    Updated button

--- js/script.js
+++ js/script.js
@@ -2,6 +2,12 @@ var button = document.querySelector(".show-cat");
 
 var cat = document.querySelector(".cat");
 
-button.addEventListener("click", function() {
-    cat.classList.toggle("show");
+button.addEventListener("click", function () {
+  if (cat.classList.contains("show")) {
+    cat.classList.remove("show");
+    button.innerText = "Wait, come back!";
+  } else {
+    cat.classList.add("show");
+    button.innerText = "Shoo cat!!";
+  }
 });
