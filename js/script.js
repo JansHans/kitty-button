13e4c045b32e676f835e859c3d28a22ec5f22aac
Jenni Hanna <79390603+JansHans@users.noreply.github.com>
2023-10-16 09:40:22 -0400

    Added cat button function

--- js/script.js
+++ js/script.js
@@ -1,2 +1,7 @@
 var button = document.querySelector(".show-cat");
-console.log(button);
+
+var cat = document.querySelector(".cat");
+
+button.addEventListener("click", function() {
+    cat.classList.toggle("show");
+});
